ffe585d2c661ec519a6f2184b959dfc5e7021e19
Alexander Dubinski <addubinski@gmail.com>
2018-04-21 17:31:20 +0900

    new

--- assets/js/components/TransactionsPage.js
+++ assets/js/components/TransactionsPage.js
@@ -16,6 +16,7 @@ import Table, {
 import Typography from 'material-ui/Typography';
 import Paper from 'material-ui/Paper';
 import Icon from 'material-ui/Icon';
+import Moment from 'react-moment';
 
 class TransactionsPage extends Component {
 
@@ -42,72 +43,98 @@ class TransactionsPage extends Component {
     }
   }
 
+  getTypeImg(type) {
+    switch (type) {
+      case 'buy':
+        return 'https://placehold.it/500x500';
+      case 'sell':
+        return 'https://placehold.it/500x500';
+      case 'send':
+        return 'https://placehold.it/500x500';
+    }
+  }
+
   getPlace(place) {
     switch (place) {
       case 'coinbase':
         return 'Coinbase Exchange'
+      case 'address':
+        return '***********fsfsdf24*********'
+    }
+  }
+
+  getDateType(type, date) {
+    let msg = null;
+    const momentDate = <Moment format="MMM DD YY">{date}</Moment>;
+    switch (type) {
+      case 'buy':
+        msg = 'Recieved';
+        break;
+      case 'sell':
+        msg = 'Sold';
+        break;
+      case 'send':
+        msg = 'Sent';
+        break;
+      default:
+        msg = 'error';
+    }
+    return (
+      <React.Fragment>
+      <Typography style={{fontSize: '14px', fontWeight: 'bold'}}>{msg || 'error'}</Typography>
+      <Typography style={{fontSize: '12px'}}>{momentDate || 'error'}</Typography>
+      </React.Fragment>
+    )
+  }
+
+  getStatus(status) {
+    switch (status) {
+      case 'pending':
+        return (
+          <React.Fragment>
+          <span style={{width: '3px', height: '3px', border: '3px solid grey', borderRadius: '3px', display: 'inline-block', marginRight: '2px'}}></span>
+          <span>Pending</span>
+          </React.Fragment>
+        )
+        case 'completed':
+        return (
+          <React.Fragment>
+          <span style={{width: '3px', height: '3px', border: '3px solid green', borderRadius: '3px', display: 'inline-block', marginRight: '2px'}}></span>
+          <span>Complete</span>
+          </React.Fragment>
+        )
     }
   }
 
 
   render() {
     const currentTransactions = this.props.transactions.map((transaction) => {
-      switch (transaction.type) {
-        case 'buy':
-          return (
-            <TableRow key={transaction.id}>
-              <TableCell>
-                <img style={this.styles.typeImg} src='https://placehold.it/500x500' alt='currency img' />
-              </TableCell>
-              <TableCell>
-                <img style={this.styles.currencyImg} src={this.getCurrencyImg(transaction.amount.currency)} alt='currency img' />
-              </TableCell>
-              <TableCell>
-                <Typography>{this.getPlace(transaction.exchange || 'address')}</Typography>
-                <Typography>Ammount</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography>place</Typography>
-                <Typography>Ammount</Typography>
-              </TableCell>
-              <TableCell>
-                <Icon>arrow_drop_down_circle</Icon>
-              </TableCell>
-            </TableRow>
-          )
-        case 'sell':
-          return (
-            <TableRow key={transaction.id}>
-              <TableCell>
-                <img style={this.styles.typeImg} src='https://placehold.it/500x500' alt='currency img' />
-              </TableCell>
-              <TableCell>
-                <img style={this.styles.currencyImg} src={this.getCurrencyImg(transaction.amount.currency)} alt='currency img' />
-              </TableCell>
-              <TableCell>
-                <Typography>{this.getPlace(transaction.exchange || 'address')}</Typography>
-                <Typography>Ammount</Typography>
-              </TableCell>
-              <TableCell></TableCell>
-            </TableRow>
-          )
-        case 'send':
-          return (
-            <TableRow key={transaction.id}>
-              <TableCell>
-                <img style={this.styles.typeImg} src='https://placehold.it/500x500' alt='currency img' />
-              </TableCell>
-              <TableCell>
-                <img style={this.styles.currencyImg} src={this.getCurrencyImg(transaction.amount.currency)} alt='currency img' />
-              </TableCell>
-              <TableCell>
-                <Typography>{this.getPlace(transaction.exchange || 'address')}</Typography>
-                <Typography>Ammount</Typography>
-              </TableCell>
-              <TableCell></TableCell>
-            </TableRow>
-          )
-      }
+      return (
+        <TableRow key={transaction.id}>
+          <TableCell padding='checkbox'>
+            <img style={this.styles.typeImg} src={this.getTypeImg(transaction.type)} alt='currency img' />
+          </TableCell>
+          <TableCell>
+            <span>
+              {this.getDateType(transaction.type, transaction.created_at)}
+              {<Typography>{this.getStatus(transaction.status)}</Typography>}
+            </span>
+          </TableCell>
+          <TableCell>
+            <img style={this.styles.currencyImg} src={this.getCurrencyImg(transaction.amount.currency)} alt='currency img' />
+          </TableCell>
+          <TableCell>
+            <Typography style={{fontSize: '12px'}}>{this.getPlace(transaction.exchange || 'address')}</Typography>
+            <Typography style={{fontSize: '12px', fontWeight: 'bold'}}>{`${transaction.amount.amount} ${transaction.amount.currency}`}</Typography>
+            {
+              transaction.type === 'sold' ? this.getLossProfit() : null
+            }
+          </TableCell>
+          <TableCell>
+            <Icon>arrow_drop_down_circle</Icon>
+          </TableCell>
+        </TableRow>
+      )
     });
 
 
