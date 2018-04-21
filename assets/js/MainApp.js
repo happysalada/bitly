f1a2884288c223714420b6a5ceacc23b402104fe
happysalada <raphael.megzari@gmail.com>
2018-04-21 20:29:12 +0900

    remove last traces of crystal

--- assets/js/MainApp.js
+++ assets/js/MainApp.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
 import HomePage from './components/HomePage';
 import TransactionsPage from './components/TransactionsPage';
 import WalletPage from './components/WalletPage';
 import WidgetPage from './components/WidgetPage';
 
-import { withRouter } from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
 
 class MainApp extends Component {
   render() {
@@ -13,15 +13,6 @@ class MainApp extends Component {
       <React.Fragment>
         <Route path="/app/" exact component={HomePage} />
         <Route path="/app/transactions" exact component={TransactionsPage} />
-        <Route path="/app/widget" exact component={WidgetPage} />
-        <Route path="/app/dashboard" exact component={Dashboard} />
-        <Route path="/app/profile" component={UserProfile} />
-        <Route path="/app/forms" component={Forms} />
-        <Route path="/app/tables" component={Tables} />
-        <Route path="/app/maps" component={MapsPage} />
-        <Route path="/app/charts" component={Charts} />
-        <Route path="/app/calendar" component={Calendar} />
-        <Footer />
       </React.Fragment>
     );
   }
