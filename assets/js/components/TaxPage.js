6a4126a48f621e88e24be43161f9e11dc4f050a2
Alexander Dubinski <addubinski@gmail.com>
2018-04-22 12:42:03 +0900

    new

--- assets/js/components/TaxPage.js
+++ assets/js/components/TaxPage.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
-import {withRouter} from 'react-router-dom';
-import {connect} from 'react-redux';
-import {compose} from 'redux';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Button from 'material-ui/Button';
@@ -37,7 +37,7 @@ class TaxPage extends Component {
   componentDidUpdate() {
     if (this.props.accounts && !this.props.transactions && this.state.update) {
       this.props.getTransactions(this.props.accounts);
-      this.setState({update: false});
+      this.setState({ update: false });
     }
   }
 
@@ -45,59 +45,80 @@ class TaxPage extends Component {
   render() {
     const incomes = Object.entries(this.props.incomes).map(currency => {
       return (
-        <TableRow style={{fontSize: '14px'}} key={currency[0]}>
-            <TableCell style={{fontSize: '14px'}}>{currency[0]}</TableCell>
-            <TableCell style={{fontSize: '14px'}}>{currency[1]}</TableCell>
+        <TableRow style={{ fontSize: '14px' }} key={currency[0]}>
+          <TableCell style={{ fontSize: '14px' }}>{currency[0]}</TableCell>
+          <TableCell style={{ fontSize: '14px' }}>{currency[1]}</TableCell>
         </TableRow>
       )
     });
 
     const totalTaxes = {};
     Object.entries(this.props.incomes).forEach(currency => {
-      totalTaxes[currency[0]] = currency[1]*0.15;
+      totalTaxes[currency[0]] = currency[1] * 0.15;
     });
 
     const viewTaxes = Object.entries(totalTaxes).map(currency => {
       return (
-        <TableRow style={{fontSize: '14px'}} key={currency[0]}>
-            <TableCell style={{fontSize: '14px'}}>{currency[0]}</TableCell>
-            <TableCell style={{fontSize: '14px'}}>{currency[1]}</TableCell>
+        <TableRow style={{ fontSize: '14px' }} key={currency[0]}>
+          <TableCell style={{ fontSize: '14px' }}>{currency[0]}</TableCell>
+          <TableCell style={{ fontSize: '14px' }}>{currency[1]}</TableCell>
         </TableRow>
       )
     });
 
     return (
       <React.Fragment>
-      <AppBar position='static' style={{margin: 0}}>
-      <Toolbar disableGutters>
-        <span style={{ flex: 1 }}></span>
-        <Typography color='secondary' variant='title' style={{ fontSize: '30px' }}>Taxes</Typography>
-        <span style={{ flex: 1 }}></span>
-      </Toolbar>
-    </AppBar>
-    <NavTabs />
-      <Paper style={{padding: '10px', margin: 'auto', width: '75%'}}>
-      <Table>
-          <TableBody>
-          <TableRow>
-          <TableCell style={{fontWeight: 'bold', fontSize: '16px'}}>Profits:</TableCell>
-          </TableRow>
-            {incomes}
-          </TableBody>
+        <AppBar position='static' style={{ margin: 0 }}>
+          <Toolbar disableGutters>
+            <span style={{ flex: 1 }}></span>
+            <Typography color='secondary' variant='title' style={{ fontSize: '30px' }}>Taxes</Typography>
+            <span style={{ flex: 1 }}></span>
+          </Toolbar>
+        </AppBar>
+        <NavTabs />
+        <Paper style={{ padding: '10px', margin: 'auto', width: '75%' }}>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell style={{ fontWeight: 'bold', fontSize: '16px' }}>Profits:</TableCell>
+              </TableRow>
+              {incomes}
+            </TableBody>
           </Table>
-      </Paper>
-        <Paper style={{padding: '10px', margin: '30px auto', width: '75%'}}>
-        <Table>
-          <TableBody>
-        <TableRow>
-          <TableCell style={{fontWeight: 'bold', fontSize: '16px'}}>Taxes Owed:</TableCell>
-          </TableRow>
-            {viewTaxes}
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell style={{ fontWeight: 'bold', fontSize: '16px' }}>USD Profits:</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell style={{ fontSize: '14px', backgroundColor: '#fff8ad', paddingLeft: '10px' }}>{`$${this.props.usdIncome.toFixed(2)} USD`}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Paper>
+        <Paper style={{ padding: '10px', margin: '30px auto', width: '75%' }}>
+          
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell style={{ fontWeight: 'bold', fontSize: '16px' }}>Taxes Owed: </TableCell>
+              </TableRow>
+              {viewTaxes}
+            </TableBody>
+          </Table>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell style={{ fontWeight: 'bold', fontSize: '16px' }}>USD Taxes Owed:</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell style={{ fontSize: '14px', backgroundColor: '#fff8ad', paddingLeft: '10px' }}>{`$${(this.props.usdIncome*0.15).toFixed(2)} USD`}</TableCell>
+              </TableRow>
             </TableBody>
           </Table>
         </Paper>
 
-    </React.Fragment>
+      </React.Fragment>
     );
   }
 }
@@ -107,7 +128,8 @@ const mapStateToProps = state => {
     transactions: state.transactions,
     wallets: state.wallets,
     accounts: state.accounts,
-    incomes: state.income
+    incomes: state.income,
+    usdIncome: state.usdIncome
   };
 };
 
