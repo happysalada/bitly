05a14db0f1d2c317fbbde081a2c1a71d59c9ecd5
happysalada <raphael.megzari@gmail.com>
2018-04-22 11:15:44 +0900

    add deployment informations

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :bitduty, BitdutyWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "http", host: "shielded-headland-82070.herokuapp.com", port: 80],
+  url: [scheme: "https", host: "tranquil-caverns-74382.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
